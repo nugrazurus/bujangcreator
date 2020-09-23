421f1c4ece20ebe75b2f54378e801c18a3199d1f
nugrazurus <nugrazurus@student.untan.ac.id>
2020-09-24 06:22:42 +0700

    add navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,117 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              aria-label="Main menu"
+              aria-expanded="false"
+            >
+              <svg
+                className="block h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className="hidden h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0">
+              <img
+                className="block lg:hidden h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-on-dark.svg"
+                alt="Workflow logo"
+              />
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg"
+                alt="Workflow logo"
+              />
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex">
+                <a
+                  href="#"
+                  className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  Dashboard
+                </a>
+                <a
+                  href="#"
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  Team
+                </a>
+                <a
+                  href="#"
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  Projects
+                </a>
+                <a
+                  href="#"
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                >
+                  Calendar
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden sm:hidden">
+        <div className="px-2 pt-2 pb-3">
+          <a
+            href="#"
+            className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >
+            Dashboard
+          </a>
+          <a
+            href="#"
+            className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >
+            Team
+          </a>
+          <a
+            href="#"
+            className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >
+            Projects
+          </a>
+          <a
+            href="#"
+            className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >
+            Calendar
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
---
+++ src/screen/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../components/navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container mx-auto">
+        <h1 className="text-6xl">
+          PARTNER IT <br />
+          PROFESIONAL ANDA
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eaque
+          molestiae explicabo quas iusto quasi sunt! Suscipit quo minima vero,
+          modi mollitia nihil. Eaque totam ut dicta eum dolorem adipisci.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Home;
