58e2a87658b0639223535b6d3c74b4cd5232fd16
nugrazurus <nugrazurus@student.untan.ac.id>
2020-09-23 20:44:09 +0700

    Updates

---
+++ precache-manifest.82eba1dd89ab45c986a76843ec7b82c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91432204b6fc8f17beb0a2e3900a8ee1",
+    "url": "/bujangcreator/index.html"
+  },
+  {
+    "revision": "be219a37c96f5c9649e2",
+    "url": "/bujangcreator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53213037d0a378df0068",
+    "url": "/bujangcreator/static/js/2.91e94be6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bujangcreator/static/js/2.91e94be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be219a37c96f5c9649e2",
+    "url": "/bujangcreator/static/js/main.5b4fcb12.chunk.js"
+  },
+  {
+    "revision": "906425fdc7501a72d2ee",
+    "url": "/bujangcreator/static/js/runtime-main.44373f30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bujangcreator/static/media/logo.5d5d9eef.svg"
+  }
+]);
